1d5f6ab83d6037d79e1840d9128e431ffd8abcba
Alok-2003 <alok953280@gmail.com>
2025-04-11 19:55:32 +0530

    Add Car Tracking Parameters

---
+++ src/pages/VehicleTracking.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MapView from '../components/map-view'
+
+const VehicleTracking = () => {
+  return (
+    <div>
+        <MapView/>
+    </div>
+  )
+}
+
+export default VehicleTracking
